a2905a169ac3791a4a5890d2cf20e90429502489
clairedeboer <claire.deboer@gmail.com>
2020-06-16 17:32:33 -0400

    Add get and post requests

--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+let productsData = [{Size: 8, Color: 'Black'}, {Size: 7, Color: 'Blue'}, ]; 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (request, response) => {
+  console.log('first get request', request); 
+  console.log('first get response', response);
+  return response.send(productsData);
 });
+
+//Object.values(users)
+
+router.get('/:productId', (request, response) => {
+  console.log('second get request.params,productID', request.params.productId);
+  return response.send(productsData[request.params.productId]); 
+});
+ 
+router.post('/', (request, response) => {
+  console.log('post request', request); 
+  const product = {
+    Size: request.body.Size,
+    Color: request.body.Color, 
+  };
+  productsData.push(product); 
+  console.log('post request.body', request.body)
+  return response.send('ok');
+}); 
 
 module.exports = router;
